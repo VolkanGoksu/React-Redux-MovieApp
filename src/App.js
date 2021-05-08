38ae521c6f0debfed125bffbfe8162972c2fe28c
volkanGoksu <volkan.goksu@outlook.com>
2021-05-08 15:59:39 +0300

    footer component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@
 import React, { Component } from 'react'
 import 'semantic-ui-css/semantic.min.css';
 import {menuStyle,fixedMenuStyle} from './helpers/styleHelper'
-
+import { Link, Route } from 'react-router-dom';
 import {
   Container,
-  Divider,
-  Grid,
-  Header,
   Image,
-  List,
   Menu,
-  Segment,
   Visibility,
 } from 'semantic-ui-react'
 import MoviesPage from './components/pages/MoviesPage';
+import Footer from './components/Footer'
 
-import { Link, Route } from 'react-router-dom';
+
 
 
 
@@ -51,7 +47,7 @@ class App extends Component {
           >
             <Container text>
               <Menu.Item>
-                <Image size='mini' src='/logo.png' />
+                <Image size='mini' src='https://react.semantic-ui.com/logo.png' />
               </Menu.Item>
               <Menu.Item header>Movieapp</Menu.Item>
               <Menu.Item as={Link} to="/movies">
@@ -67,65 +63,7 @@ class App extends Component {
 
           <Route path='/movies' component={MoviesPage}></Route>
         </Container>
-
-        <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
-          <Container textAlign='center'>
-            <Grid columns={4} divided stackable inverted>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 1' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 2' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 3' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Footer Header' />
-                  <p>
-                    Extra space for a call to action inside the footer that could help re-engage
-                    users.
-                  </p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-            <Divider inverted section />
-            <Image src='/logo.png' centered size='mini' />
-            <List horizontal inverted divided link size='small'>
-              <List.Item as='a' href='#'>
-                Site Map
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Contact Us
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Terms and Conditions
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Privacy Policy
-              </List.Item>
-            </List>
-          </Container>
-        </Segment>
+         <Footer />
       </div>
     )
   }
