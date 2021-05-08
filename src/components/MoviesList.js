1f09829578766ca146e80263aa51d7e585a29110
volkanGoksu <volkan.goksu@outlook.com>
2021-05-08 15:01:06 +0300

    MoviesList Component

---
+++ src/components/MoviesList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+const MovieList = ({ movies }) => {
+    const emptyMessage = (
+        <p>No Movies yet.</p>
+    )
+
+    const moviesList = (
+        <div>
+            Movies List
+        </div>
+    )
+    return (
+        <div>
+            {movies.length === 0 ? emptyMessage : moviesList}
+        </div>
+    )
+}
+
+MovieList.propTypes = {
+    movies: PropTypes.array.isRequired
+}
+export default MovieList
