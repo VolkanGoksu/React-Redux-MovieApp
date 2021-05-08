b43c326a9af54b831b1a81ffe54f35aa1b576f90
volkanGoksu <volkan.goksu@outlook.com>
2021-05-08 15:36:41 +0300

    semantic layout

--- src/components/pages/MoviesPage.js
+++ src/components/pages/MoviesPage.js
@@ -1,24 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import MoviesList from '../MoviesList';
+
+
 class MoviesPage extends Component {
     static propTypes = {
-        movies:PropTypes.array.isRequired
+        movies: PropTypes.array.isRequired
     }
-	render() {
-        console.log(this.props);
-		return (
-			<div>
-				<h2>Movies Page</h2>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <h2>Movies Page</h2>
+                <MoviesList movies={this.props.movies}></MoviesList>
+            </div>
+        );
+    }
 }
 
-const mapStateToProps = ({movies}) =>{
-    return{
-         movies
+    const mapStateToProps = ({ movies }) => {
+        return {
+            movies
+        }
     }
-}
-export default connect(mapStateToProps)(MoviesPage);
+    export default connect(mapStateToProps)(MoviesPage);
